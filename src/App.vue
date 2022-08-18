6aa6f215c0aba83a82ca5173b7d51ee4e4436db0
Levi <otterbach.levi@gmail.com>
2022-08-18 13:52:26 +0200

    try to init repo - has bugs

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-wrapper">
+    <CourseSidebarMenu />
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <div class="router-view-wrapper">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <a> hello world</a>
+  </div>
+</template>
+
+
