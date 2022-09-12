3bdeefec896bc39fd6481c4af0677e59ce4507e1
Martin Lautenschlager <102482387+marlau97@users.noreply.github.com>
2022-09-12 16:45:49 +0200

    fix alert for error

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -130,7 +130,7 @@ function nextQuestion() {
       initialQuestionCount.value
     );
     postGameResult(result).catch((reason) => {
-      alert(reason.data.message());
+      alert(reason.response.message);
     });
     resetValues();
     showEndscreen.value = true;
